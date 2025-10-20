ce90f6c1eade7eab7a17b6d9bff37dcba10bd80d
Vincent-Caraga <vincentccaraga@gmail.com>
2025-10-20 16:00:58 +0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./Components/Auth/LoginPage.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { Pool } = require("pg"); // Import Pool from pg
+require("dotenv").config(); //Load environent variables from .env files
+
+//Database Configuration (POSTGRESQL)
+// Connection Logic
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+// Test query
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Database connection error", err);
+  } else {
+    console.log("Database connected:", res.rows[0].now);
+  }
+});
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json()); //Allows server to read JSON data from the client side
+
+//Test Route
+app.get("/", (req, res) => {
+  res.send("Server is running! Ready for appointments.");
+});
+
+//A. GET ALL DOCTORS ROUTE
+app.get("/api/doctors", async (req, res) => {
+  try {
+    const allDoctors = await pool.query(
+      "SELECT doctor_id, first_name, last_name, specialty FROM doctors"
+    );
+    res.json(allDoctors.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//B. GET ALL APPOINTMENTS (Sample POST)
+app.post("/api/appointments", async (req, res) => {
+  try {
+    //Assume you are sending patient_id, doctor_id, date, and reason from the frontend
+    const { patient_id, doctor_id, appointment_date, reason } = req.body;
+
+    const newAppointment = await pool.query(
+      "INSERT INTO appointments (patient_id, doctor_id, appointment_date, reason) VALUES ($1, $2, $3, $4) RETURNING *",
+      [patient_id, doctor_id, appointment_date, reason]
+    );
+    res.json(newAppointment.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Failed to create appointment");
+  }
+});
+
+//START SERVER
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+//Last edit Oct 16 and okay na rito with database connection and proceed to frontend

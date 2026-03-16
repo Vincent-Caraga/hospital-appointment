e5a44b6094255456b3722981790b9fd72b2d2bca
Vincent-Caraga <vincentccaraga@gmail.com>
2026-03-16 16:49:01 +0800

    feat: api for appointment

--- server/index.js
+++ server/index.js
@@ -64,24 +64,41 @@ app.get("/api/doctors", async (req, res) => {
   }
 });
 
-//B. GET ALL APPOINTMENTS (Sample POST)
+//B. CREATE APPOINTMENT
 app.post("/api/appointments", async (req, res) => {
   try {
-    //Assume you are sending patient_id, doctor_id, date, and reason from the frontend
-    const { patient_id, doctor_id, appointment_date, reason } = req.body;
+    const { patient_name, doctor_id, appointment_date, appointment_time } =
+      req.body;
 
     const newAppointment = await pool.query(
-      "INSERT INTO appointments (patient_id, doctor_id, appointment_date, reason) VALUES ($1, $2, $3, $4) RETURNING *",
-      [patient_id, doctor_id, appointment_date, reason],
+      `INSERT INTO appointments (patient_name, doctor_id, appointment_date, appointment_time, status) VALUES ($1, $2, $3, $4, 'pending') RETURNING *`,
+      [patient_name, doctor_id, appointment_date, appointment_time],
     );
     res.json(newAppointment.rows[0]);
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Failed to create appointment");
+    console.error("Booking Error", err.message);
+    res.status(500).json({ error: "Failed to create appointment" });
   }
 });
 
-//C. UPDATE USER PROFILE
+//C. CHECK BOOKED SLOTS
+// This will check by React if user choose a date on a calendar
+app.get("/api/appointments/booked/:doctorId/:date", async (req, res) => {
+  try {
+    const { doctorId, date } = req.params;
+    const result = await pool.query(
+      "SELECT appointment_time FROM appointments WHERE doctor_id = $1 AND appointment_date = $2 AND status != 'cancelled'",
+      [doctorId, date],
+    );
+    //Return of array for time
+    res.json(result.rows.map((row) => row.appointment_time));
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//D. UPDATE USER PROFILE
 app.put("/api/profile/:id", async (req, res) => {
   //Get the user ID from the URL parameter
   const { id } = req.params;

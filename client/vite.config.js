039f080723956bc195534e9352353c2fa33c353c
Vincent-Caraga <vincentccaraga@gmail.com>
2026-04-16 17:17:11 +0800

    final vite config fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    //This tells Vite to proxy requests starting with /api/
-    //to your Express server running on http://localhost:5000
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false, //Set to true if using HTTPS
-      },
-    },
-  },
 });
